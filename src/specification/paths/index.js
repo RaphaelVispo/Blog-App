0dc5dd8b64e581bb5df9f6d6c16120dfa01652d4
Raphael Vispo <rsvispo@up.edu.ph>
2022-12-28 12:18:02 +0000

    Add: configure for open api specs

--- src/specification/paths/index.js
+++ src/specification/paths/index.js
@@ -1,10 +1,12 @@
 import { blog } from './blog/index.js';
+import { comment } from './comment/index.js';
 import { general } from './general/index.js';
 import { user } from './user/index.js';
 
 export const paths = {
   ...general,
   ...user,
-  ...blog
+  ...blog,
+  ...comment
 
 };
