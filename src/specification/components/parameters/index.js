63853f0ca5afaa1136237c4bb17f58fbc8fcffeb
Raphael Vispo <rsvispo@up.edu.ph>
2022-12-29 15:24:23 +0000

    Add: config for get user data for open api

--- src/specification/components/parameters/index.js
+++ src/specification/components/parameters/index.js
@@ -17,5 +17,14 @@ export const parameters = {
     schema: {
       type: 'string'
     }
+  },
+  UserParameterId: {
+    name: 'userId',
+    in: 'path',
+    required: true,
+    description: 'This is the id of the user',
+    schema: {
+      type: 'string'
+    }
   }
 };
--- src/specification/paths/index.js
+++ src/specification/paths/index.js
@@ -1,12 +1,14 @@
 import { blog } from './blog/index.js';
 import { comment } from './comment/index.js';
 import { general } from './general/index.js';
+import { userData } from './user-data/index.js';
 import { user } from './user/index.js';
 
 export const paths = {
   ...general,
   ...user,
   ...blog,
-  ...comment
+  ...comment,
+  ...userData
 
 };
